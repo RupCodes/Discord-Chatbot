9fe38b04a9bc0c0a26961c3b3c6e535747544840
Gogeta <78647871+TheRealGogeta@users.noreply.github.com>
2021-05-21 08:20:35 +0530

    Update chatSend.js

--- util/chatSend.js
+++ util/chatSend.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 require("./inline.js");
 
 const chatSend = async (message) => {
+   try {
     let bid = process.env.bid // Your Bid From Brainshop.ai
     let key = process.env.key // Your Key From Brainshop.ai
     let uid = "1"
@@ -10,12 +11,20 @@ const chatSend = async (message) => {
         message.channel.startTyping()
        await axios.get(`http://api.brainshop.ai/get?bid=${bid}&key=${key}&uid=${uid}&msg=${msg}`)
        .then(res => {
-       let reply = res.data;
+       let data = res.data;
+      let reply = data.cnt
         if (reply) {
             message.channel.stopTyping();
             message.sendInline(reply.cnt, { allowedMentions: { repliedUser: false } });
-        }
+        } else if(!reply) {
+        message.channel.stopTyping();
+        message.sendInline("no reply from the api", { allowedMentions: { repliedUser: false } });
+}
        })
+ } catch (e) {
+        message.channel.stopTyping();
+        console.log(e);
+    }
 };
 
 module.exports = {
